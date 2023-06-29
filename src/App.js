406e14443a4167d178415039dae12eb67a1b0400
Prakhar Sharma <prakhar@clinikk.com>
2023-06-29 12:37:28 +0530

    Login functionality

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
+function LoginForm(){
+  const [formData, setFormData] = useState({
+    name: '',
+    password: ''
+  });
+
+  function handleChange(event){
+    setFormData({...formData, [event.target.name]:event.target.value})
+  }
+  
+  function handleLogin(event){
+    event.preventDefault();
+    console.log(formData);
+  }
+  
+  function LoginButton(){
+    return <button type='submit'>Log In</button>
+  }
+
+  return (
+    <form onSubmit={handleLogin}>
+          <div className="form-group">
+            <label htmlFor="username">Username:</label>
+            <input type="text" id="username" name="name" placeholder="Enter your username" value={formData.name} onChange={handleChange} required></input>
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" name="password" placeholder="Enter your password" value={formData.password} onChange={handleChange} required></input>
+          </div>
+          <LoginButton />
+    </form>
+  )
+}
+
+  
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          Login to see magic
+        <div className="login-form">
+        <h2>Login</h2>
+        <LoginForm />
+      </div>
       </header>
     </div>
   );
